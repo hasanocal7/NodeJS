b1a9a2d8585ad34d88504501bd3e3aa6f4fc6154
hasan <hasanocal7@gmail.com>
2023-09-16 20:40:33 +0300

    File System Module

---
+++ fsmodule.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+// DOSYA OKUMA (ReadFile)
+
+/*
+
+fs.readFile('hasan.txt', 'utf8', (err, data) => { // callback fonksiyonu ile birlikte çalıştırıyoruz.
+    if (err) console.log(err);                         // hata kontrolü
+    console.log(data);                                 // okunan verinin çıktısının alınması
+})
+
+// DOSYA OKUMA (WriteFile)
+
+fs.writeFile('example.json', '{"name": "Arin", "age": 6}', 'utf8', (err) => {
+    if (err) console.log(err);
+});
+
+// VERİ EKLEME
+
+fs.appendFile('hasan.txt', '\ntaksimcenk', 'utf-8', (err) => {
+    if (err) {
+        console.log(err);
+    }
+});
+
+// DOSYA SİLME
+
+fs.unlink('example.json', (err) => {
+    if (err) console.log(err);
+});
+
+// KLASÖR OLUŞTURMA
+
+fs.mkdir('cenk/taksimharitasi', { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
+
+// KLASÖR SİLME
+
+fs.rmdir('cenk', { recursive: true }, (err) => {
+    if (err) console.log(err);
+})
+
+*/
+
+
+
+
+
+
+
